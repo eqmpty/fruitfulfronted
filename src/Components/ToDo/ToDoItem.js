73d1b29e0822b07ddde895439fa40db5c5c3ebb8
Zhenya <evgeniyagorshchyk@gmail.com>
2020-06-03 01:38:51 +0300

    c

--- src/Components/ToDo/ToDoItem.js
+++ src/Components/ToDo/ToDoItem.js
@@ -4,21 +4,22 @@ import instance from '../../services/tokenprovider';
 
 export class ToDoItem extends Component {
 
-    // constructor(props){
-    //     super(props);
-    //     this.state = {
-    //         currentToDo:{}
-    //     }
-    // }
+    constructor(props){
+        super(props);
+        this.state = {
+            currentToDo:{}
+        }
+    }
 
-    // componentDidMount(){
-    //     const ToDoItem = (this.props.match.params.todoid);
-    //     instance.get('getcurrentuser/' + ToDoItem)
-    //     .then(result => {
-    //         this.setState({currentToDo:result.data})
-    //         console.log(result)
-    //     })
-    // }
+    componentDidMount(){
+        const ToDoItem = (this.props.match.params.todoid);
+        instance.get('getcurrentuser/' + ToDoItem)
+        .then(result => {
+            this.setState({currentToDo:result.data})
+            console.log(result)
+        })
+    }
+    
     render(){
         return(
             <Fragment>
