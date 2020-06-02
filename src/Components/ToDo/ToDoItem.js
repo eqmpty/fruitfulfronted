b1fc2741bafd7c0a5b1e3127ef8665ec77989b72
Zhenya <evgeniyagorshchyk@gmail.com>
2020-06-03 00:45:53 +0300

    comment

--- src/Components/ToDo/ToDoItem.js
+++ src/Components/ToDo/ToDoItem.js
@@ -1,6 +1,24 @@
 import React, {Component, Fragment} from 'react';
+import {withRouter} from 'react-router-dom';
+import instance from '../../services/tokenprovider';
 
 export class ToDoItem extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            currentToDo:{}
+        }
+    }
+
+    componentDidMount(){
+        const ToDoItem = (this.props.match.params.todoid);
+        instance.get('getcurrentuser/' + ToDoItem)
+        .then(result => {
+            this.setState({currentToDo:result.data})
+            console.log(result)
+        })
+    }
     render(){
         return(
             <Fragment>
@@ -10,4 +28,4 @@ export class ToDoItem extends Component {
     }
 }
 
-export default ToDoItem;
+export default withRouter(ToDoItem);
