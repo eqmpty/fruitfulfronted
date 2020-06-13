31498cc26f7a1f5e01a15d77f2206bd28e8497e5
Zhenya <evgeniyagorshchyk@gmail.com>
2020-06-13 12:11:11 +0300

    todo validation

--- src/Components/ToDo/ToDoForm.js
+++ src/Components/ToDo/ToDoForm.js
@@ -10,17 +10,38 @@ export default class ToDoForm extends Component {
         this.state = {
             text: '',
             complete:false,
-            description:''
+            description:'',
+            textError:''
         }
     }
 
     enterToDo = event => {
         this.setState({ [event.target.name]: event.target.value })
     }
+
+    validate = () => {
+        let isError = false;
+        const errors = {
+            textError:'',
+        }
+
+        if (this.state.text.length === 0) {
+            isError = true;
+            errors.textError = "this field must be filled";
+        }
+
+        this.setState({
+            ...this.state,
+            ...errors
+        });
+        
+        return isError;
+    }
     
     sumbitToDo = event => {
         console.log(this.state);
         event.preventDefault();
+        const err = this.validate();
 
         this.setState({
             text:'',
@@ -28,19 +49,20 @@ export default class ToDoForm extends Component {
             description:''
         });
 
-        instance.post('addtodo', this.state)
+        if(!err) {instance.post('addtodo', this.state)
         .then (response => {
             console.log (response)
         })
         .catch (error => {
             console.log('!Error ' + error);
         })
+    }
         
         
     }
 
     render(){
-        const {text, description} = this.state;
+        const {text, description, textError} = this.state;
         return (
             <Fragment>
                 <div className = 'container'>
@@ -49,6 +71,7 @@ export default class ToDoForm extends Component {
                             <form onSubmit = {this.sumbitToDo} className = 'addToDoForm'>
                                 <p id = 'addNewTask'> add new task </p>
                                 <input name = 'text' value = {text} onChange = {this.enterToDo} placeholder = 'to do...'/>
+                                <p className = 'error'> {textError} </p>
                                 <input id= 'descriptionInput' name = 'description' value = {description} onChange = {this.enterToDo} placeholder = 'tell more about it...'/>
                                 <button onChange = {this.sumbitToDo} id = 'addToDoButton'> add </button>
                             </form>
