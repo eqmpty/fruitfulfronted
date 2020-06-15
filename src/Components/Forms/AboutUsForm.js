f18196febfdc1bc6bc97921470c49087abf49f0c
Zhenya <evgeniyagorshchyk@gmail.com>
2020-06-15 14:55:47 +0300

    ab page

---
+++ src/Components/Forms/AboutUsForm.js
@@ -0,0 +1,28 @@
+import React, { Component, createContext, Fragment } from 'react'
+import { Link } from 'react-router-dom';
+import './FormsStyles.css'
+
+export class AboutUsForm extends Component {
+    render() {
+        return (
+            <Fragment>
+                <div className = 'container'>
+                    <div className = 'row'>
+                        <form className = 'offset-lg-3 col-lg-6'>
+                            <p id = 'addNewTask'>about us</p>
+                            <p>
+                                App which helps you to be more productive. This app is an application 
+                                that gives ability for people to make a To-Do List, set goals and be 
+                                more fruitful. You will have opportunity to check you statistics and 
+                                state of your tasks . Also you can see other users,subscribe to them  
+                                and comment their notes.
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+export default AboutUsForm; 
--- src/Pages/AboutUsPage.js
+++ src/Pages/AboutUsPage.js
@@ -1,12 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import UserNavBarVS from '../Components/Layouts/UserNavBarSV';
 import NavBarVS from '../Components/Layouts/NavBarSV';
+import AboutUsForm from '../Components/Forms/AboutUsForm';
 
 export class AboutUsPage extends Component {
     render(){
         return (
             <Fragment>
                 {localStorage.getItem('user') ? <UserNavBarVS /> : <NavBarVS />}
+                <AboutUsForm />
             </Fragment>
         )
     }
